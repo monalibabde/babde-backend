c386f9a261fc3c904e462504ecee308b7a493da3
monalibabde <monalibabde@gmail.com>
2025-05-13 09:25:32 +0530

    utlis code

--- src/index.js
+++ src/index.js
@@ -13,7 +13,17 @@ dotenv.config({
 });
 
 // Now call connectDB() to initiate the MongoDB connection
-connectDB();
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`@ server is running at port: ${process.env.PORT}`);
+        
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection failed !!! ", err);
+    
+})
 
 
 
